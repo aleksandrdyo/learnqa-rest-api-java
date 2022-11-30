aea5bcd6ed1dea92d4f0ac3ec46589d0ad16ca8f
Dyo Aleksandr <dyoaleksandr@MacBook-Pro-Dyo.local>
2022-12-01 01:47:15 +0500

    test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,16 @@
+import io.restassured.RestAssured;//импорт класса RestAssured
+import io.restassured.response.Response;//импорт класса
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("HelloWorld");
+
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")//создание get запроса на нужный апи
+                .andReturn();//возврат результата
+        response.prettyPrint();//вывод текста ответа в удобном формате
+
     }
 }
