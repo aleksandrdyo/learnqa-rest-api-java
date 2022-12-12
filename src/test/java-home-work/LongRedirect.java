1857ea45f6fe8940055a91f3516e8297be8946e7
Dyo Aleksandr <dyoaleksandr@Dyo.local>
2022-12-13 00:26:12 +0500

    fix long_redirect task

--- src/test/java-home-work/LongRedirect.java
+++ src/test/java-home-work/LongRedirect.java
@@ -8,8 +8,11 @@ public class LongRedirect {
 
         String link = "https://playground.learnqa.ru/api/long_redirect";
         int redirectCount = 0;
+        int status_code = 0;
 
-        for(int i = 0; i<5; i++){
+        //for(int i = 0; i<5; i++)
+        while(status_code!=200)
+        {
 
             Response response = RestAssured
                     .given()
@@ -19,15 +22,19 @@ public class LongRedirect {
                     .get(link);
                     //.andReturn();
 
-            int status_code = response.getStatusCode();
-            System.out.println("Статус кода - " + status_code);
+            int get_status_code = response.getStatusCode();
+            System.out.println("Статус кода - " + get_status_code);
             System.out.println("______");
             String locationHeader = response.getHeader("location");
             link=locationHeader;
             redirectCount=redirectCount+1;
+
+            status_code = get_status_code;
+
             if (status_code==200){
                 break;
             }
+
             System.out.println("URL - " + locationHeader);
             System.out.println("Редирект №-" + redirectCount);
 
