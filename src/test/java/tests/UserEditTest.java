2283edb99ad6b6d65be96f8e7cbe07f1072df6bb
Dyo Aleksandr <dyoaleksandr@Dyo.local>
2022-12-29 18:34:01 +0500

    added negative test for edit user

--- src/test/java/tests/UserEditTest.java
+++ src/test/java/tests/UserEditTest.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import lib.BaseTestCase;
 import lib.Assertions;
 import lib.DataGenerator;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -16,7 +17,6 @@ public class UserEditTest extends BaseTestCase {
     public void testEditJustCreatedTest() {
         //Generate User
         Map<String, String> userData = DataGenerator.getRegistrationData();
-        //System.out.println(userData);
 
         JsonPath responseCreateAuth = RestAssured
                 .given()
@@ -24,9 +24,8 @@ public class UserEditTest extends BaseTestCase {
                 .post("https://playground.learnqa.ru/api/user/")
                 .jsonPath();
 
-        //responseCreateAuth.prettyPrint();
         String userId = responseCreateAuth.getString("id");
-        //System.out.println(userId);
+        //System.out.println("posit " + userId);
 
         //Login
         Map<String, String> authData = new HashMap<>();
@@ -40,7 +39,7 @@ public class UserEditTest extends BaseTestCase {
                 .post("https://playground.learnqa.ru/api/user/login")
                 .andReturn();
 
-        responseGetAuth.prettyPrint();
+        //responseGetAuth.prettyPrint();
 
         //Edit
         String newName = "Changed Name";
@@ -63,10 +62,135 @@ public class UserEditTest extends BaseTestCase {
                 .get("https://playground.learnqa.ru/api/user/" + userId)
                 .andReturn();
 
-        //responseGetData.prettyPrint();
+        //responseUserData.prettyPrint();
         //System.out.println(responseGetData.asString());
         Assertions.assertJsonByName(responseUserData, "firstName", newName);
+    }
 
+    //Ex17: Негативные тесты на PUT
+    @Test
+    public void testEditNegative() {
+        //Generate User
+        Map<String, String> userData1 = DataGenerator.getRegistrationData();
+
+        JsonPath responseCreateAuth = RestAssured
+                .given()
+                .body(userData1)
+                .post("https://playground.learnqa.ru/api/user/")
+                .jsonPath();
+
+        String userId1 = responseCreateAuth.getString("id");
+
+        //Login
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", userData1.get("email"));
+        authData.put("password", userData1.get("password"));
+
+        Response responseGetAuth1 = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        //Generate Other User With Other Id-
+        Map<String, String> userDataOther = DataGenerator.getRegistrationData();
+
+        JsonPath responseCreateAuthOther = RestAssured
+                .given()
+                .body(userDataOther)
+                .post("https://playground.learnqa.ru/api/user/")
+                .jsonPath();
+
+        String userIdOther = responseCreateAuthOther.getString("id");
+
+        //Login Other User With Other Id-
+        Map<String, String> authData1 = new HashMap<>();
+        authData1.put("email", userDataOther.get("email"));
+        authData1.put("password", userDataOther.get("password"));
+
+        Response responseGetAuth2 = RestAssured
+                .given()
+                .body(authData1)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+
+        //- Попытаемся изменить данные пользователя, будучи неавторизованными
+        //Edit
+        String newName = "Changed Name";
+        Map<String, String> editData = new HashMap<>();
+        editData.put("firstName", newName);
+
+        Response responseEditUserWithoutAuth = RestAssured
+                .given()
+                .body(editData)
+                .put("https://playground.learnqa.ru/api/user/" + userId1)
+                .andReturn();
+
+        Assertions.assertResponseTextEquals(responseEditUserWithoutAuth, "Auth token not supplied");
+        Assertions.assertResponseCodeEquals(responseEditUserWithoutAuth, 400);
+
+        //- Попытаемся изменить данные пользователя, будучи авторизованными другим пользователем
+        //Edit
+        String newNameOther = "Changed Name123";
+        Map<String, String> editDataOther = new HashMap<>();
+        editDataOther.put("firstName", newNameOther);
+
+        Response responseEditOtherUser1 = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth1, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth1, "auth_sid"))
+                .body(editDataOther)
+                .put("https://playground.learnqa.ru/api/user/"+userIdOther)
+                .andReturn();
+
+        responseEditOtherUser1.prettyPrint();
+        //Assertions.assertResponseTextEquals(responseEditOtherUser1, "");
+        Assertions.assertResponseCodeEquals(responseEditOtherUser1, 400);
+
+        //- Попытаемся изменить данные пользователя, будучи авторизованными другим пользователем
+        //проверка на изменение данных
+        Response responseUserData2 = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth1, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth1, "auth_sid"))
+                .get("https://playground.learnqa.ru/api/user/" + userId1)
+                .andReturn();
+
+        //- Попытаемся изменить email пользователя, будучи авторизованными тем же пользователем,
+        // на новый email без символа @
+        //Edit
+        String newEmail = "vinkotovexample.ru";
+        Map<String, String> editEmail = new HashMap<>();
+        editEmail.put("email", newEmail);
+
+        Response responseEditUserWithIncorrectEmail = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth2, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth2, "auth_sid"))
+                .body(editEmail)
+                .put("https://playground.learnqa.ru/api/user/" + userIdOther)
+                .andReturn();
+
+        Assertions.assertResponseTextEquals(responseEditUserWithIncorrectEmail, "Invalid email format");
+        Assertions.assertResponseCodeEquals(responseEditUserWithIncorrectEmail, 400);
+
+        //- Попытаемся изменить firstName пользователя, будучи авторизованными тем же пользователем,
+        // на очень короткое значение в один символ
+        String newShortName = "v";
+        Map<String, String> editFname = new HashMap<>();
+        editFname.put("firstName", newShortName);
+
+        Response responseEditUserWithShortName = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth2, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth2, "auth_sid"))
+                .body(editFname)
+                .put("https://playground.learnqa.ru/api/user/" + userIdOther)
+                .andReturn();
+
+        Assertions.assertJsonByName(responseEditUserWithShortName,"error", "Too short value for field firstName");
+        Assertions.assertResponseCodeEquals(responseEditUserWithIncorrectEmail, 400);
 
     }
 }
